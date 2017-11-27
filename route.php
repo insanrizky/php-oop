c60ead8a60812dc6c36debc620210f4604e932a7
Insan Rizky <insansan9@gmail.com>
2017-11-27 08:51:34 +0700

    Controller

--- route.php
+++ route.php
@@ -1,16 +1,4 @@
 <?php
 include "config/autoload.php";
-$model = binding(["user"]);
 
-switch($_GET['user']){
-  case 'save':
-    if($_POST['id']){
-      $model->user->update($_POST);
-    }else{
-      $model->user->save($_POST);
-    }
-  case 'delete':
-    $model->user->delete($_GET['id']);
-  default:
-    header('location: ./');
-}
+include "controller/user.php";
